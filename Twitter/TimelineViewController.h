b57fd83072849173edafc8ecf09af72c6bbed737
Tripta Gupta <gupta.tripta@gmail.com>
2014-03-27 22:28:39 -0700

    more bare bones

---
+++ Twitter/TimelineViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimelineViewController.h
+//  Twitter
+//
+//  Created by Tripta Gupta on 3/27/14.
+//  Copyright (c) 2014 Tripta Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimelineViewController : UIViewController
+
+@end
