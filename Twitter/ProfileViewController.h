ec0a39400b79e77a907b07286d2166225d466d1d
Tripta Gupta <gupta.tripta@gmail.com>
2014-04-03 19:58:21 -0700

    set up xib for ProfileViewController

---
+++ Twitter/ProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.h
+//  Twitter
+//
+//  Created by Tripta Gupta on 4/3/14.
+//  Copyright (c) 2014 Tripta Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "User.h"
+
+@interface ProfileViewController : UIViewController
+
+//@property (nonatomic, strong) User *user;
+
+@end
---
+++ Twitter/ProfileViewController.m
@@ -0,0 +1,47 @@
+//
+//  ProfileViewController.m
+//  Twitter
+//
+//  Created by Tripta Gupta on 4/3/14.
+//  Copyright (c) 2014 Tripta Gupta. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+#import <UIImageView+AFNetworking.h>
+
+@interface ProfileViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *userProfileImage;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *twitterHandleLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *tweetCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *followingCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *followersCountLabel;
+
+@end
+
+@implementation ProfileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"Profile";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
